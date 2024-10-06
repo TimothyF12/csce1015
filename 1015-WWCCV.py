607b928be62278e4e6a7dbe50b55e2449eaccf73
Timothy <TimothyFoster2@my.unt.edu>
2024-10-06 15:25:52 -0500

    Timothy Foster implemented luhn and testing

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -17,6 +17,19 @@ print ("Timothy Foster Credit Card Validator")
 card_number = input("Enter your 16-digit credit card number: ")
 
 # Validate the credit card number using the Luhn algorithm
+def run_tests():
+
+    assert is_credit_card_valid("4111111111111111"), '4111111111111111 should pass but did not'
+
+    assert is_credit_card_valid("5105105105105100"), '5105105105105100 should pass but did not'
+
+    assert not is_credit_card_valid("134"), '134 should not pass but did'
+
+    assert not is_credit_card_valid("1234567890123456"), '1234567890123456 should not pass but did'
+
+    assert not is_credit_card_valid("000000000000"), 'This is a bad test and we will get an error message'
+
+run_tests() 
 
 if is_credit_card_valid(card_number):           # we are calling the function above and sending it the card_number to validate
 
@@ -25,3 +38,5 @@ if is_credit_card_valid(card_number):           # we are calling the function ab
 else:
 
     print("The credit card number is invalid.")
+
+    
